ede6c3ce2c39c60f8e6a4ce3f88059c3f29767b9
dinamashmush <dina.Mashmush@carmel6000.amitnet.org>
2022-09-20 09:16:48 +0300

    .

--- main/index.js
+++ main/index.js
@@ -1,32 +1,32 @@
-const express = require('express')
-const enterRouter = require('./routes/enter')
-const errorRouter = require('./routes/error')
-const bodyParser = require('body-parser');
-const homeRouter = require('./routes/home');
-const commentRouter = require('./routes/comment')
-const app = express();
+// const express = require('express')
+// const enterRouter = require('./routes/enter')
+// const errorRouter = require('./routes/error')
+// const bodyParser = require('body-parser');
+// const homeRouter = require('./routes/home');
+// const commentRouter = require('./routes/comment')
+// const app = express();
 
-const port = 8000;
+// const port = 8000;
 
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-
-const browserMiddleware = require('./middlewares/browser-middleware')
-
-app.use(browserMiddleware);
-app.use(homeRouter);
-app.use('/comment', commentRouter)
-app.use('/error', errorRouter)
-app.use('/enter', enterRouter)
+// app.use(bodyParser.json());
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: true,
+//     })
+// );
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
 
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+// const browserMiddleware = require('./middlewares/browser-middleware')
+
+// app.use(browserMiddleware);
+// app.use(homeRouter);
+// app.use('/comment', commentRouter)
+// app.use('/error', errorRouter)
+// app.use('/enter', enterRouter)
+
+
+// app.listen(port, () => {
+//     console.log(`App listening on port ${port}`)
+// })
